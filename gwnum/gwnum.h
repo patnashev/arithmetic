c266e57af92ae72ad60920733d91f5b88391c4f4
patnashev <atn@timus.ru>
2026-07-03 18:20:54 +0500

    GWNum 31.06

--- gwnum/gwnum.h
+++ gwnum/gwnum.h
@@ -16,7 +16,7 @@
 | threads IF AND ONLY IF each uses a different gwhandle structure
 | initialized by gwinit or gwclone.
 | 
-|  Copyright 2002-2025 Mersenne Research, Inc.  All rights reserved.
+|  Copyright 2002-2026 Mersenne Research, Inc.  All rights reserved.
 +---------------------------------------------------------------------*/
 
 #ifndef _GWNUM_H
@@ -55,9 +55,9 @@ typedef gwnum *gwarray;
 /* are new prime95 versions without any changes in the gwnum code.  This version number is also embedded in the assembly code and */
 /* gwsetup verifies that the version numbers match.  This prevents bugs from accidentally linking in the wrong gwnum library. */
 
-#define GWNUM_VERSION		"31.3"
+#define GWNUM_VERSION		"31.6"
 #define GWNUM_MAJOR_VERSION	31
-#define GWNUM_MINOR_VERSION	3
+#define GWNUM_MINOR_VERSION	6
 
 /* Error codes returned by the three gwsetup routines */
 
@@ -986,6 +986,7 @@ struct gwhandle_struct {
 	char	GLOBAL_MULBYCONST;	/* Internal flag used to support deprecated gwsetnormroutine c parameter.  Use GWMUL_MULBYCONST instead. */ 
 	char	GLOBAL_POSTFFT;		/* Internal flag used to support deprecated gwstartnextfft routine.  Use GWMUL_STARTNEXTFFT instead. */ 
 	char	POSTFFT;		/* Internal flag indicating the current multiply operation should start the forward FFT on the result */
+	char	smaller_fftlen_count;	/* Internal count of skipped smaller FFT lengths.  Used in implementing larger_fftlen_count. */
 	int	FFT_TYPE;		/* Home-grown, Radix-4, etc. */
 	int	ARCH;			/* Architecture.  Which CPU type the FFT is optimized for. */
 	void	(*GWPROCPTRS[15])(void*); /* Ptrs to assembly routines */
